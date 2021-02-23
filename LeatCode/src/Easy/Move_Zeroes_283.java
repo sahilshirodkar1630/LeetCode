e952660a658f9563db61a3042f7eec401166b99e
sahilshirodkar1630 <sahilshirodkar1630@gmail.com>
2021-02-23 18:37:18 +0530

    283. Move Zeroes

--- LeatCode/src/Easy/Move_Zeroes_283.java
+++ LeatCode/src/Easy/Move_Zeroes_283.java
@@ -17,6 +17,7 @@ public class Move_Zeroes_283 {
 	
 	 public static void moveZeroes(int[] nums) {
         int k=0;
+        
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
                 int temp = nums[k];
